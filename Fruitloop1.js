214b21095a77026ab76c7a6ac340e3c26a7eae59
mike-port <miike.day@gmail.com>
2021-04-02 16:15:47 +0900

    Fruitloops2

--- Fruitloop1.js
+++ Fruitloop1.js
@@ -1,10 +1,18 @@
+/*Fruitloop2
+
+Repeat Fruitloop1, but for each fruit name that you display, on the next line display the number of periods that is equal to the number of the index of the array.
+
+Example: "Banana" . //one period
+
+"Orange" .. //two periods etc. */
+
 let くだもの	 = [
-'はらり',
-'カキ',
-'パパイヤ',
-'きいちご',
-'ざくろ',
-'ゆず'
+'はらり', ".",
+'カキ', "..",
+'パパイヤ', "...",
+'きいちご', "....",
+'ざくろ', ".....",
+'ゆず', "......"
 ];
 
 for (let i = 0; i < くだもの.length; i++) {
